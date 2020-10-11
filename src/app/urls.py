373a3ab7b907c3261c5d6ba2fe73822d9baa3710
Javier Ramos <jaramosperez85@gmail.com>
2020-10-10 22:14:39 -0300

    Base and Home Ok

--- src/app/urls.py
+++ src/app/urls.py
@@ -2,9 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from core import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomePageView.as_view(), name='home'),
 ]
 
 if settings.DEBUG:
